c4fed1b88ad049a044fe8069ee0ebdea055ce9d9
Jessica Doss <doss.jessicam@gmail.com>
2019-08-09 13:48:25 -0700

    added more UI

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 2019_08_09_161427) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "cost"
     t.string "country_of_origin"
+    t.decimal "cost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
