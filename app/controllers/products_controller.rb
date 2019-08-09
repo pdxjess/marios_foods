ea45423a3d14455ea294b29157aa62b9066c4569
Jessica Doss <doss.jessicam@gmail.com>
2019-08-09 14:39:47 -0700

    add update and destroy methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,11 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(params_permit)
     if @product.save
+      flash[:notice] = "Product successfully added"
       redirect_to products_path
     else
-      render :new_product_path
+      flash[:notice] = "Problem adding product"
+      render :new
     end
   end
 
@@ -25,9 +27,18 @@ class ProductsController < ApplicationController
   end
 
   def update
+    if @product.update(params_permit)
+      flash[:notice] = "Product successfully updated"
+      redirect_to products_path
+    else
+      flash[:notice] = "Problem updating product"
+      render :edit
+    end
   end
 
   def destroy
+    @product.delete
+    redirect_to products_path
   end
 
   private
