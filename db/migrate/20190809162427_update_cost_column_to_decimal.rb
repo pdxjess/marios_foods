09e82423bb2c73a574ebb48add3fdcda32cad3e6
Jessica Doss <doss.jessicam@gmail.com>
2019-08-19 09:42:34 -0700

    add database migrations

--- db/migrate/20190809162427_update_cost_column_to_decimal.rb
+++
@@ -1,5 +0,0 @@
-class UpdateCostColumnToDecimal < ActiveRecord::Migration[5.2]
-  def change
-    change_column :products, :cost, :decimal
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_09_161427) do
+ActiveRecord::Schema.define(version: 2019_08_19_153944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
+    t.integer "cost"
     t.string "country_of_origin"
-    t.decimal "cost"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.string "content_body"
+    t.integer "raiting"
+    t.integer "product_id"
   end
 
 end
